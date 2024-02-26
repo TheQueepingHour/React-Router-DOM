c9d78c29ad5af0753c2cd69c3f81eabfdaafb8f5
Josh Rivera <beps@Mactop.local>
2024-02-26 17:21:27 -0600

    Added context folder and DataContext.js, as well as initialized DataContext

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react'
 import './App.css';
+import Gallery from './Components/Gallery';
+import SearchBar from './Components/SearchBar';
 
 function App() {
+  let [search, setSearch] = useState('')
+  let [message, setMessage] = useState('Search for music!')
+  let [data, setData] = useState([])
+
+  const API_URL = 'https://itunes.apple.com/search?term='
+
+  useEffect(() => {
+    if(search) {
+      const fetchData = async () => {
+        document.title = `${search}'s Music`
+        const response = await fetch(API_URL + search)
+        const resData = await response.json()
+        if (resData.results.length > 0) {
+          setData(resData.results)
+        } else {
+          setMessage('Not Found')
+        }
+      }
+      fetchData()
+    }
+  }, [search])
+
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    setSearch(term)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar handleSearch = {handleSearch} />
+      {message}
+      <Gallery data={data} />
     </div>
   );
 }
---
+++ src/Components/SearchBar.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+
+function SearchBar(props) {
+    // let [searchTerm, setSearchTerm] = useState('')
+
+    return(
+        <form>
+            <input type='text' placeholder="Enter a search term here" onChange={(e) => props.handleSearch(e, e.target.value)} />
+            <input type='submit' />
+        </form>
+    )
+}
+
+export default SearchBar
